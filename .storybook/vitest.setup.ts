0eb5a97222b9e42cc357943aeaa86e63e1e53f76
loevray <rei980213@gmail.com>
2025-05-31 21:19:48 +0900

    Chore: storybook 8 > 9 update - reduce build speed (avg 15s => 3s)

--- .storybook/vitest.setup.ts
+++ .storybook/vitest.setup.ts
@@ -1,9 +1,11 @@
 import { beforeAll } from 'vitest';
-import { setProjectAnnotations } from '@storybook/experimental-nextjs-vite';
+import { setProjectAnnotations } from '@storybook/nextjs-vite';
 import * as projectAnnotations from './preview';
 
 // This is an important step to apply the right configuration when testing your stories.
 // More info at: https://storybook.js.org/docs/api/portable-stories/portable-stories-vitest#setprojectannotations
 const project = setProjectAnnotations([projectAnnotations]);
+
+console.log('ㅎㅎㅎㅎㅎㅎㅎㅇㅇㅇㅇㅇㅇㅇ');
 
 beforeAll(project.beforeAll);
--- src/components/festival/FestivalTag.tsx
+++ src/components/festival/FestivalTag.tsx
@@ -4,11 +4,12 @@ import { cn } from '@/lib/utils';
 
 /* 
   태그마다 색의 종류가 달라야 함.
-  어드민이 축제를 추가할 수 있는 페이지가 필요함.
-  축제 추가할때 색상 선택해서 넣을 수 있게 해야함. => 나중에
+  태그 생성시 색상 선택해서 넣을 수 있게...
   
-  comic,game,all 태그는 어드민 페이지에서 직접 입력해서 추가하는 방식 x
-  custom만 직접 추가
+  장르무관, 게임, 만화 태그는 배경 색상 정해두기.
+  custom만 직접 추가.
+  
+  db에서 뽑아와야할것 같다.
 */
 
 const FestivalTagVariants = cva('px-2', {
@@ -27,15 +28,22 @@ interface I_MappedFestivalType {
   all: '장르무관';
   comic: '만화';
   game: '게임';
-  custom: '사용자 지정';
 }
 
 const MAPPED_FESTIVAL_TYPE: I_MappedFestivalType = {
   all: '장르무관',
   comic: '만화',
   game: '게임',
-  custom: '사용자 지정',
 };
+
+/*
+  1. 태그를 db에서 받아온다.
+  2. 태그 별 색상이 달라야한다. => 태그 마다 특정되는 배경색이 있어야 함.
+    2-1 태그들이 정해져있어야하나? or 개발자가 어드민페이지에서 추가할 수 있어야 하나?
+    2-2 만약 태그들이 정해져있지 않다면, 추가할 때 배경색상도 정해야함. => 배경색 정보도 db에 같이 저장해야할까?
+    
+    => 싹다 
+*/
 
 export default function FestivalTag({
   festivalType = 'comic',
@@ -45,7 +53,7 @@ export default function FestivalTag({
 }) {
   return (
     <Badge className={cn(FestivalTagVariants({ festivalType }))} {...props}>
-      {MAPPED_FESTIVAL_TYPE[festivalType] ?? '사용자 설정'}
+      {MAPPED_FESTIVAL_TYPE[festivalType] ?? '-'}
     </Badge>
   );
 }
