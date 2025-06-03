1706e893577892c6edac1054a62f410b3448be4a
loevray <rei980213@gmail.com>
2025-06-03 23:49:00 +0900

    Design: add ellipsis, styling

--- src/components/festival/FestivalCard.tsx
+++ src/components/festival/FestivalCard.tsx
@@ -1,23 +1,38 @@
 import { Card, CardContent, CardHeader } from '../ui/card';
 import FestivalTag from './FestivalTag';
+interface I_FestivalCard {
+  title: string;
+  thumbnailSrc: string;
+  dateRange: {
+    start: string;
+    end: string;
+  };
+  address: string;
+}
 
-export default function FestivalCard() {
+export default function FestivalCard({
+  title,
+  thumbnailSrc,
+  dateRange,
+  address,
+}: I_FestivalCard) {
   return (
-    <Card className="w-full max-w-sm pt-0 overflow-hidden">
+    <Card className="w-full max-w-sm py-0 overflow-hidden gap-0">
       <CardHeader className="px-0">
         {/* eslint-disable-next-line @next/next/no-img-element */}
         <img
           className="w-full aspect-video object-cover"
-          src="https://comicw.co.kr/data/item/1748337413/11_7J6E7Iuc7Ys7Iqk7YSw_6riw67O4.png"
+          src={thumbnailSrc}
           alt="festival thumbnail"
         />
       </CardHeader>
-      <CardContent className="flex flex-col gap-1">
-        <h1 className="font-bold">코믹월드 SUMMER 2025</h1>
-        <span className="text-xs">2025.07.19 ~ 2025.07.20</span>
-        <section className="flex justify-between">
-          <span className="block text-xs">일산 킨텍스 제1전시장</span>
+      <CardContent className="flex flex-col gap-0.5 pt-2 pb-4">
+        <h1 className="font-bold truncate">{title}</h1>
+        <span className="text-xs truncate">{`${dateRange.start} ~ ${dateRange.end}`}</span>
+        <section className="flex items-end justify-between gap-2">
+          <address className="text-xs truncate flex-1">{address}</address>
           <FestivalTag label="만화" color="bg-red-500" />
+          <FestivalTag label="장르 무관" color="bg-cyan-500" />
         </section>
       </CardContent>
     </Card>
