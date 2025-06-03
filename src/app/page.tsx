d08088b2277a1c16888ecd9ba4fddeb4ba6f026f
loevray <rei980213@gmail.com>
2025-06-03 23:49:00 +0900

    Design: add ellipsis, styling

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,7 +9,12 @@ export default function Page() {
       <Link href="/about">About</Link>
       <FestivalTag label="만화" color="bg-red-500" />
 
-      <FestivalCard />
+      <FestivalCard
+        title="코믹월드 SUMMER 2025"
+        dateRange={{ start: '2025.07.19', end: '2025.07.20' }}
+        address="일산 킨텍스 제1전시장"
+        thumbnailSrc="https://comicw.co.kr/data/item/1748337413/11_7J6E7Iuc7Ys7Iqk7YSw_6riw67O4.png"
+      />
     </div>
   );
 }
