dc8d3a24a7144c126e3593f79b498a247160400d
loevray <rei980213@gmail.com>
2025-06-04 21:45:41 +0900

    Refactor: render of iterate festivaltag in festivalcard component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,20 +1,19 @@
-import FestivalCard from '@/components/festival/FestivalCard';
-import FestivalTag from '@/components/festival/FestivalTag';
 import Link from 'next/link';
+
+export const DUMMY_TAG_DATA = [
+  {
+    key: 'comic',
+    label: '만화',
+    color: 'bg-red-500',
+  },
+  { key: 'all', label: '장르무관', color: 'bg-cyan-500' },
+];
 
 export default function Page() {
   return (
     <div>
       <h1>Home</h1>
       <Link href="/about">About</Link>
-      <FestivalTag label="만화" color="bg-red-500" />
-
-      <FestivalCard
-        title="코믹월드 SUMMER 2025"
-        dateRange={{ start: '2025.07.19', end: '2025.07.20' }}
-        address="일산 킨텍스 제1전시장"
-        thumbnailSrc="https://comicw.co.kr/data/item/1748337413/11_7J6E7Iuc7Ys7Iqk7YSw_6riw67O4.png"
-      />
     </div>
   );
 }
--- src/components/festival/FestivalCard.stories.tsx
+++ src/components/festival/FestivalCard.stories.tsx
@@ -1,5 +1,6 @@
 import type { Meta, StoryObj } from '@storybook/nextjs-vite';
 import FestivalCard from './FestivalCard';
+import { DUMMY_TAG_DATA } from '@/app/page';
 
 const meta = {
   title: 'Festival/FestivalCard',
@@ -45,6 +46,7 @@ export const Default: Story = {
     address: '일산 킨텍스 제1전시장',
     thumbnailSrc:
       'https://comicw.co.kr/data/item/1748337413/11_7J6E7Iuc7Ys7Iqk7YSw_6riw67O4.png',
+    tagData: DUMMY_TAG_DATA,
   },
 };
 
@@ -54,5 +56,6 @@ export const Truncated: Story = {
     dateRange: { start: '2025.07.19', end: '2025.07.20' },
     address: '여기서 저기로 가면 저기서 여기로 가고 여기서 저기로',
     thumbnailSrc: 'https://picsum.photos/600/400',
+    tagData: DUMMY_TAG_DATA,
   },
 };
