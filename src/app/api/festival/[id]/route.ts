13b9aec9a1a6d2d11ba11daf22a079d9f9d3c1a2
loevray <rei980213@gmail.com>
2025-06-12 03:57:50 +0900

    Feat: update festival api(server)

--- src/app/api/festival/[id]/route.ts
+++ src/app/api/festival/[id]/route.ts
@@ -22,7 +22,6 @@ export async function GET(
     }
 
     if (!event) {
-      // 해당 ID의 이벤트가 없을 경우 404 Not Found 응답
       return Response.json({ error: 'Event not found' }, { status: 404 });
     }
 
@@ -33,7 +32,53 @@ export async function GET(
   }
 }
 
-export async function DELETE(request: Request) {
+export async function PATCH(
+  request: Request,
+  { params }: { params: Promise<{ id: string }> }
+) {
+  const { id } = await params;
+  const body = await request.json();
+  try {
+    const supabase = await createClient();
+    const { data: event, error } = await supabase
+      .from('events')
+      .update(body)
+      .eq('id', id)
+      .select();
+
+    if (error) {
+      console.error('Error update event by ID:', error);
+      return Response.json({ error: error.message }, { status: 500 });
+    }
+
+    if (!event) {
+      // 해당 ID의 이벤트가 없을 경우 404 Not Found 응답
+      return Response.json({ error: 'Event not found' }, { status: 404 });
+    }
+
+    return Response.json(event);
+  } catch (err) {
+    console.error('Failed to update event:', err);
+    return Response.json({ error: 'Failed to update event' }, { status: 500 });
+  }
+}
+
+export async function DELETE(
+  request: Request,
+  { params }: { params: Promise<{ id: string }> }
+) {
   //특정 행사 삭제
-  return request;
+  const { id } = await params;
+
+  try {
+    const supabase = await createClient();
+    const response = await supabase.from('events').delete().eq('id', id);
+    if (response.error) {
+      return Response.json({ error: response.error }, { status: 400 });
+    }
+    return Response.json({ status: 204 });
+  } catch (err) {
+    console.error('Failed to delete event:', err);
+    return Response.json({ error: 'Failed to delete event' }, { status: 500 });
+  }
 }
