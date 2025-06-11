8016cc2c84d1aa141141ad07c15f06f75a0b59f4
loevray <rei980213@gmail.com>
2025-06-12 06:07:16 +0900

    Fix: create, get festival api (server) - add region property when get festival list - event <-> category mapping when create festival

--- src/app/api/festival/route.ts
+++ src/app/api/festival/route.ts
@@ -1,4 +1,4 @@
-import { EventSchema } from '@/schema/zod/events';
+import { CreateEventSchema } from '@/schema/zod/events';
 import { createClient } from '@/utils/supabase/server';
 
 const EVENTS_TABLE = 'events';
@@ -10,14 +10,17 @@ poster_image_url,
 description,
 start_datetime,
 end_datetime,
-location,
-region_id
+location
 `;
 
 const EVENTS_CATEGORIES_QUERY = `
 event_categories(
 categories(id,name)
 )`;
+
+const EVENTS_REGION_QUERY = `
+regions(name)
+`;
 
 export async function GET() {
   try {
@@ -26,7 +29,8 @@ export async function GET() {
       .from(EVENTS_TABLE)
       .select(
         `${EVENTS_LIST_QUERY},
-        ${EVENTS_CATEGORIES_QUERY}
+        ${EVENTS_CATEGORIES_QUERY},
+        ${EVENTS_REGION_QUERY}
       `
       )
       .order('created_at', { ascending: false });
@@ -34,44 +38,65 @@ export async function GET() {
     if (error) {
       return Response.json({ error: error.message }, { status: 500 });
     }
-    //행사목록 하나도 없을때는 프론트엔드에서 처리
 
-    //서비스로직?
-    const eventsWithCategories = events.map((event) => ({
-      ...event,
-      event_categories: event.event_categories.map((ec) => ec.categories),
-    }));
+    const transformedEvents = events.map((event) => {
+      const { regions, ...rest } = event;
+      const flatCategories = event.event_categories.map((ec) => ec.categories);
 
-    return Response.json(eventsWithCategories);
+      return {
+        ...rest,
+        event_categories: flatCategories,
+        region: regions?.name,
+      };
+    });
+
+    return Response.json(transformedEvents);
   } catch {
     return Response.json({ error: 'Failed to fetch events' }, { status: 500 });
   }
 }
 
 export async function POST(request: Request) {
-  //태그도 받아와야 함
-  //event categori목록을 db에서 쿼리 후 fe에 제공, 그 값을 기반으로 태그를 fe에서 be로 넘겨줘야 무결성이 보장된다.
-
-  const json = await request.json();
-  const parsed = EventSchema.safeParse(json);
+  const body = await request.json();
+  const parsed = CreateEventSchema.safeParse(body);
 
   if (!parsed.success) {
     return Response.json(parsed.error.flatten(), { status: 400 });
   }
 
+  const { region_id, category_ids, ...eventData } = parsed.data;
+
   try {
     const supabase = await createClient();
 
-    const { data, error } = await supabase
-      .from(EVENTS_TABLE)
-      .upsert(parsed.data);
+    const { data: eventInsert, error: eventError } = await supabase
+      .from('events')
+      .insert([{ ...eventData, region_id }])
+      .select()
+      .single();
 
-    if (error) {
-      return Response.json({ error: error.message }, { status: 500 });
+    if (eventError) {
+      return Response.json({ error: eventError.message }, { status: 500 });
     }
 
-    return Response.json(data, { status: 201 });
+    const eventCategoriesToInsert = category_ids.map((category_id) => ({
+      event_id: eventInsert.id,
+      category_id: category_id,
+    }));
+
+    const { error: eventCategoryMappingError } = await supabase
+      .from('event_categories')
+      .insert(eventCategoriesToInsert);
+
+    if (eventCategoryMappingError) {
+      return Response.json(
+        { error: 'Failed to created event connect to categories' },
+        { status: 500 }
+      );
+    }
+
+    return Response.json(eventInsert, { status: 201 });
   } catch {
-    return Response.json({ error: 'Failed to create events' }, { status: 500 });
+    return Response.json({ error: 'Failed to create event' }, { status: 500 });
   }
 }
