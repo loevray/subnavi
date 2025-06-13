1b05c1abd4c91c2296c250c00883dfe8b434b78f
loevray <rei980213@gmail.com>
2025-06-14 05:10:19 +0900

    Feat: main page skeleton when fetch datas (suspense fallback)

---
+++ src/app/loading.stories.tsx
@@ -0,0 +1,18 @@
+import type { Meta, StoryObj } from '@storybook/nextjs-vite';
+import Loading from './loading';
+
+const meta = {
+  title: 'Skeletons/MainPageSuspensed',
+  component: Loading,
+
+  parameters: {
+    layout: 'centered',
+  },
+  tags: ['autodocs'],
+  argTypes: {},
+} satisfies Meta<typeof Loading>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Default: Story = { args: {} };
