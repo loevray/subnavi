98e5cb15fc9a418d473b452cdf295e6c39e49568
loevray <rei980213@gmail.com>
2025-06-24 08:56:30 +0900

    Remove: remove search page

--- src/app/search/page.tsx
+++
@@ -1,9 +0,0 @@
-export default async function SearchPage({
-  searchParams,
-}: {
-  searchParams: Promise<{ keyword: string }>;
-}) {
-  const { keyword } = await searchParams;
-  console.log(keyword);
-  return <div>{/* <EventList /> */}</div>;
-}
