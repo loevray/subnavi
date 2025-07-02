19bc14cd81303ff5430aee0187bd75e1a54b7f60
loevray <rei980213@gmail.com>
2025-07-02 19:24:47 +0900

    Fix: hotfix!!!

--- src/components/event/eventFilter/EventFilterContainer.tsx
+++ src/components/event/eventFilter/EventFilterContainer.tsx
@@ -1,9 +1,7 @@
 import { categoryService } from '@/services/Category';
 import EventFilter from './EventFilter';
-import { sleep } from '@/utils/sleep';
 
 export default async function EventFilterContainer() {
   const categories = await categoryService.getCateogires();
-  await sleep();
   return <EventFilter categories={categories} />;
 }
