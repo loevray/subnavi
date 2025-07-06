b3aece35c4a7d05ec73729ba63e0065ba4257542
loevray <rei980213@gmail.com>
2025-07-06 19:29:14 +0900

    Fix: event detail page

--- src/app/event/[id]/page.tsx
+++ src/app/event/[id]/page.tsx
@@ -1,13 +1,10 @@
 import EventDetailView from './EventDetailView';
 import { eventService } from '@/services/Event';
 
-export default async function EventDetail({
-  params,
-}: {
-  params: Promise<{ id: string }>;
-}) {
+export default async function EventDetail({ params }: { params: Promise<{ id: string }> }) {
   const { id } = await params;
-  const event = await eventService.getEventById({ id });
+  const response = await eventService.getEventById({ id });
+  if (!response.success) return <div>이벤트 상세페이지 fetching중 에러</div>;
 
-  return <EventDetailView event={event} />;
+  return <EventDetailView event={response.data} />;
 }
