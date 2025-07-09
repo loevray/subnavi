cca3711de9fd473a8f9dd2b8eddc391257ac1916
loevray <rei980213@gmail.com>
2025-07-09 21:00:21 +0900

    Refactor: cateogries api

--- src/app/api/categories/route.ts
+++ src/app/api/categories/route.ts
@@ -1,23 +1,10 @@
-import { isCustomError } from '@/lib/errors/serviceErrors.server';
 import { categoryService } from '@/services/Category';
 
 export async function GET() {
-  try {
-    const data = categoryService.getCateogires();
-    return Response.json(data);
-  } catch (error) {
-    console.log('Failed to fetch categories', error);
+  const serviceResponse = await categoryService.getCateogires();
+  if (serviceResponse.success) return Response.json(serviceResponse.data);
+  const { error } = serviceResponse;
 
-    if (isCustomError(error)) {
-      return Response.json(
-        {
-          error: error.message,
-          details: error.details,
-        },
-        { status: error.statusCode }
-      );
-    }
-
-    return Response.json({ error: 'Internal Server Error' }, { status: 500 });
-  }
+  console.error('Error in GET /api/categories:', error);
+  return Response.json({ error }, { status: error.statusCode });
 }
