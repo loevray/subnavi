bfcfe63a37d54a2dcbc6a180ec3e0c8644c7bf33
loevray <rei980213@gmail.com>
2026-03-17 10:43:20 +0900

    remove: storybook-static

--- storybook-static/assets/EventCard.stories-CEtBViHI.js
+++
@@ -1,5 +0,0 @@
-import{j as e}from"./iframe-EtnJgSCB.js";import{E as n,D as d}from"./EventCard-BwtQLyNC.js";import"./preload-helper-Dp1pzeXC.js";import"./EventTag-Gm8M1l9j.js";const l={title:"Event/EventCard",component:n,parameters:{layout:"centered"},decorators:[s=>e.jsx("div",{style:{width:"400px",height:"300px"},children:e.jsx(s,{})})],tags:["autodocs"],argTypes:{title:{control:"text",description:"event title"},posterImageUrl:{control:"text",description:"event thumbnail image URL"},dateRange:{description:"event start date, end date",control:"object"},address:{control:"text",description:"event address"}}},t={args:{...d[0]}};var r,o,a;t.parameters={...t.parameters,docs:{...(r=t.parameters)==null?void 0:r.docs,source:{originalSource:`{
-  args: {
-    ...DUMMY_EVENT_CARD_DATA[0]
-  }
-}`,...(a=(o=t.parameters)==null?void 0:o.docs)==null?void 0:a.source}}};const x=["Default"];export{t as Default,x as __namedExportsOrder,l as default};
